45de18df7aa3b09dde1e946ca50f8dc096d6af3e
Burstniart <v4gr33n@hotmail.com>
2022-05-17 22:38:12 -0500

    Add Pet component to create elements in App

---
+++ AdoptMe/src/App.js
@@ -0,0 +1,32 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name ),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.color),
+      ]);
+};
+
+const App = () => {
+    return React.createElement(
+      "div",
+      {},[
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+          name: "Hanzo",
+          animal: "Cat",
+          color: "Gray & White"
+        }),
+        React.createElement(Pet, {
+          name: "Hamtaro",
+          animal: "Hamster",
+          color: "Orange"
+        }),
+        React.createElement(Pet, {
+          name: "Bambi",
+          animal: "Dog",
+          color: "Black, brown & White"
+        }),
+      ]
+    );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
