7adcf8750a19fb7b743c6e9446b98970787c2a55
Burstniart <v4gr33n@hotmail.com>
2022-05-18 16:21:14 -0500

    Implemented a hook, hooray!

--- AdoptMe/src/SearchParams.js
+++ AdoptMe/src/SearchParams.js
@@ -1,11 +1,14 @@
+import { useState } from "react"; // this is a hook
+
 const SearchParams = () => {
-  const location = "Gto. Mexico";
+  // const location = "Gto. Mexico";
+  const [location, setLocation] = useState("")
   return (
     <div className="search-params">
-      <form>
+      <form >
         <label htmlFor="location">
-          Location
-          <input id="location" value={location} placeholder="Location"/>
+          Location 
+          <input id="location" placeholder="Location"/>
         </label>
         <button>Submit</button>
       </form>
