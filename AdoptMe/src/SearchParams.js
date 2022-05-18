e9e7e8e262ee52b9f8e4ccb5f4cf7dacb386a79d
Burstniart <v4gr33n@hotmail.com>
2022-05-18 16:53:43 -0500

    Correct eslint plugin name to jsx-a11y

--- AdoptMe/src/SearchParams.js
+++ AdoptMe/src/SearchParams.js
@@ -2,17 +2,17 @@ import { useState } from "react"; // this is a hook
 
 const SearchParams = () => {
   // const location = "Gto. Mexico";
-  const [location, setLocation] = useState("")
+  const [location, setLocation] = useState("");
   return (
     <div className="search-params">
-      <form >
+      <form>
         <label htmlFor="location">
-          Location 
-          <input id="location" placeholder="Location"/>
+          Location
+          <input id="location" placeholder="Location" />
         </label>
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 export default SearchParams;
