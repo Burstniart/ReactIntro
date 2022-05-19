593b8ce9518854e1f83ca8294adc8925ae3d6aba
Burstniart <v4gr33n@hotmail.com>
2022-05-18 23:08:32 -0500

    Add code for custom hook for breeds

---
+++ AdoptMe/useBreedList.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const localCahe = {};
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCahe[animal]) {
+      setBreedList(localCahe[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      )
+      const json = await res.json();
+      localCahe[animal] = json.breeds || [];
+      setBreedList(localCahe[animal]);
+      setStatus("loaded");
+    }
+  }, [animal])
+  return [breedList,status]
+}
